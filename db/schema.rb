639c9b6cbd106026f4c6f2fb205b35408f2a58c7
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-27 17:33:06 -0500

    add associations to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724141255) do
+ActiveRecord::Schema.define(version: 20160226221558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "arts", force: :cascade do |t|
+    t.string   "author"
+    t.string   "born_died"
+    t.string   "title"
+    t.string   "date"
+    t.string   "technique"
+    t.string   "location"
+    t.string   "url"
+    t.string   "form"
+    t.string   "style"
+    t.string   "school"
+    t.string   "timeframe"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -27,4 +43,17 @@ ActiveRecord::Schema.define(version: 20150724141255) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["token"], name: "index_users_on_token", unique: true, using: :btree
 
+  create_table "votes", force: :cascade do |t|
+    t.boolean  "likeDislike"
+    t.integer  "art_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "votes", ["art_id"], name: "index_votes_on_art_id", using: :btree
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
+
+  add_foreign_key "votes", "arts"
+  add_foreign_key "votes", "users"
 end
