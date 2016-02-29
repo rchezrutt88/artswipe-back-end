2ca95b1d87f6e5570da13a568545a7400a1a7586
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-29 14:37:18 -0500

    build new vote off current_user, assign @art to new @votes

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,70 @@
+class VotesController < ProtectedController
+  before_action :set_vote, only: [:show, :update, :destroy]
+  before_action :set_art, only: [:create]
+  # GET /votes
+  # GET /votes.json
+  def index
+    @votes = Vote.all
+
+    render json: @votes
+  end
+
+  # GET /votes/1
+  # GET /votes/1.json
+  def show
+    render json: @vote
+  end
+
+  # POST /votes
+  # POST /votes.json
+
+  # FIXME
+  def create
+    # @vote = @art.votes.build(vote_params)
+    # # Why does this not need to be a reference to current_user.id
+    # @vote.user = current_user
+
+    @vote = current_user.votes.build(vote_params)
+    @vote.art = @art
+
+
+    if @vote.save
+      render json: @vote, status: :created
+    else
+      render json: @vote.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /votes/1
+  # PATCH/PUT /votes/1.json
+  def update
+
+    if @vote.update(vote_params)
+      head :no_content
+    else
+      render json: @vote.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /votes/1
+  # DELETE /votes/1.json
+  def destroy
+    @vote.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_vote
+      @vote = Vote.find(params[:id])
+    end
+
+    def set_art
+      @art = Art.find(params[:art_id])
+    end
+
+    def vote_params
+      params.require(:vote).permit(:liked)
+    end
+end
