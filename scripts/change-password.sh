439a2fe3fb21b896908def5f5523264b2308771b
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-28 23:15:37 -0500

    playing around with scripts

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 curl --include --request PATCH http://localhost:3000/change-password/$ID \
-  --header "Authorization: Token token=$TOKEN" \
+  --header "Authorization: Token token=8848d996d319c569f437864570199165" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
-      "old": "an example password",
-      "new": "super sekrit"
+      "old": "x",
+      "new": "z"
     }
   }'
--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --include --request DELETE http://localhost:3000/sign-out/$ID \
-  --header "Authorization: Token token=$TOKEN"
+curl --include --request DELETE http://localhost:3000/sign-out/1 \
+  --header "Authorization: Token token=bcb6014f0166b17c6feba2e1fa311976"
