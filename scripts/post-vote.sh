0ac8529b23879f4e66c58778e363d9c882174497
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-29 14:38:47 -0500

    write curl script for creating votes. probably doesn't work.

---
+++ scripts/post-vote.sh
@@ -0,0 +1,6 @@
+curl --include --request POST http://localhost:3000/1/votes \
+  --header "Authorization: Token token=bcb6014f0166b17c6feba2e1fa311976" \
+  --header "Content-Type: application/json" \
+  --data '{
+
+  }'
