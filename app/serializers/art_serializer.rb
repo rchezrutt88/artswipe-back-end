8a503c4dab8622c16ac5da682cffcfe92cb09cad
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-01 19:37:00 -0500

    add serializer that returns ALL votes associated with art object when that art object is returned

--- app/serializers/art_serializer.rb
+++ app/serializers/art_serializer.rb
@@ -1,6 +1,29 @@
 class ArtSerializer < ActiveModel::Serializer
-  attributes :id, :author, :born_died, :title, :date, :technique, :location, :url, :form, :style, :school, :timeframe
+  attributes
+   :id,
+   :author,
+   :born_died,
+   :title,
+   :date,
+   :technique,
+   :location,
+   :url,
+   :form,
+   :style,
+   :school,
+   :timeframe,
+   :get_votes
+
 
   has_many :votes
-  
+
+  # TODO add serializer that returns all votes!
+
+private
+
+  def get_votes
+    Vote.where(art_id: id)
+  end
+
+
 end
