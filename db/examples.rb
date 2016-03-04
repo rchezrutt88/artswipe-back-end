b6fedae3e5e91d284b7d24058303539e4b30ac71
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-04 15:55:32 -0500

    load scripts and data from master

--- db/examples.rb
+++ db/examples.rb
@@ -16,3 +16,90 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+
+require 'csv'
+
+art_path = 'data/arts.csv'
+user_path = 'data/users.csv'
+vote_path = 'data/votes.csv'
+
+def populate_arts(path)
+  Art.transaction do
+    CSV.foreach(Rails.root + path, headers: true, col_sep: ';', header_converters: :downcase) do |art_row|
+      art = art_row.to_hash
+      next if Art.exists? art
+      Art.create!(art)
+    end
+  end
+end
+
+def populate_users(path)
+  User.transaction do
+    CSV.foreach(Rails.root + path, headers: true) do |user_row|
+      user = user_row.to_hash
+      next if User.exists? user
+      User.create!(user)
+    end
+  end
+end
+
+def populate_votes(path)
+  Vote.transaction do
+    CSV.foreach(Rails.root + path, headers: true) do |vote_row|
+      vote = vote_row.to_hash
+      next if Vote.exists? vote
+      Vote.create!(vote)
+    end
+  end
+end
+
+
+populate_arts(art_path)
+
+# populate_users(user_path)
+
+
+
+
+# namespace :db do
+#   namespace :populate do
+#     desc 'Fill the database with example data'
+#     task all: [:users, :arts, :votes]
+#
+#     desc 'Fill the people table with example data'
+#     task people: :environment do
+#       Person.transaction do
+#         CSV.foreach(Rails.root + 'data/people.csv',
+#                     headers: true) do |person_row|
+#           person = person_row.to_hash
+#           next if Person.exists? person
+#           Person.create!(person)
+#         end
+#       end
+#     end
+#
+#     desc 'Fill the cities table with example data'
+#     task cities: :environment do
+#       City.transaction do
+#         CSV.foreach(Rails.root + 'data/cities.csv',
+#                     headers: true) do |city_row|
+#           city = city_row.to_hash
+#           next if City.exists? city
+#           City.create!(city)
+#         end
+#       end
+#     end
+#
+#     desc 'Fill the pets table with example data'
+#     task pets: :environment do
+#       Pet.transaction do
+#         CSV.foreach(Rails.root + 'data/pets.csv',
+#                     headers: true) do |pet_row|
+#           pet = pet_row.to_hash
+#           next if Pet.exists? pet
+#           Pet.create!(pet)
+#         end
+#       end
+#     end
+#   end
+# end
---
+++ scripts/slice-data.rb
@@ -0,0 +1,20 @@
+# to slice data file down to manageable size
+
+require 'csv'
+require 'pry'
+catalog = '../data/catalog.csv'
+catalog2 = '../data/catalog2.csv'
+
+def random_lines(input_file, output_file, num)
+  header = File.open(input_file, &:readline)
+
+  old_csv = CSV.open(input_file, 'r', col_sep: ';', headers: true)
+  new_csv = CSV.open(output_file, 'w', col_sep: ';', headers: true)
+
+  new_csv << CSV.parse_line(header, col_sep: ';')
+
+  rows = old_csv.to_a.slice(1..-1)
+  random_rows = rows.sample(num)
+
+  random_rows.each { |row| new_csv << row }
+end
