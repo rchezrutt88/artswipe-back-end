b1c79b15cb21c63a039ed7a7d34240c84e15d0a6
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-04 17:01:59 -0500

    add art routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+
+  get 'arts/random', to: 'arts#random'
+  resources :arts, only: [:index, :show]
+
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
 end
