bdb0651acb95d143dc0d8af47836099c04a88fd7
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-07 20:14:35 -0500

    slice-data script for filtering only portraits, formating headers (BORN-DIED -> BORN_DIED, TYPE -> STYLE), and selecting random 20

--- scripts/slice-data.rb
+++ scripts/slice-data.rb
@@ -2,19 +2,52 @@
 
 require 'csv'
 require 'pry'
-catalog = '../data/catalog.csv'
-catalog2 = '../data/catalog2.csv'
+catalog = File.expand_path("../catalog.csv", Dir.pwd)
+arts_csv = File.expand_path("./data/arts.csv", Dir.pwd)
 
-def random_lines(input_file, output_file, num)
-  header = File.open(input_file, &:readline)
 
-  old_csv = CSV.open(input_file, 'r', col_sep: ';', headers: true)
-  new_csv = CSV.open(output_file, 'w', col_sep: ';', headers: true)
+def format_csv(input_file, output_file, num)
 
-  new_csv << CSV.parse_line(header, col_sep: ';')
+  old_csv = CSV.read(input_file, "r:ISO-8859-1", col_sep: ';', headers: true)
 
-  rows = old_csv.to_a.slice(1..-1)
-  random_rows = rows.sample(num)
+  old_csv.delete_if do |row|
+    row['TYPE'] != 'portrait'
+  end
 
-  random_rows.each { |row| new_csv << row }
+
+  # puts old_csv
+
+  CSV.open(output_file, 'w:ISO-8859-1', col_sep: ';', headers: true) do |csv|
+    headers = old_csv.headers
+    headers[1] = 'BORN_DIED'
+    headers[8] = 'STYLE'
+    csv << headers
+    random_rows = old_csv.to_a.slice(1..-1).sample(num)
+    random_rows.each do |row|
+      csv << row
+    end
+
+  end
 end
+
+format_csv(catalog, arts_csv, 20)
+
+
+
+# def random_lines(input_file, output_file, num)
+#   header = File.open(input_file, &:readline)
+#
+#
+#   new_csv = CSV.open(output_file, 'w', col_sep: ';', headers: true)
+#
+#   new_csv << CSV.parse_line(header, col_sep: ';')
+#
+#   rows = old_csv.to_a.slice(1..-1)
+#
+#
+#   random_rows = rows.sample(num)
+#
+#   random_rows.each do |row|
+#     if
+#   end
+# end
