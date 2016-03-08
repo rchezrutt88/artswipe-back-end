a392bc175bb4a12aedf0951a462955d9d92a60c0
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-07 23:09:52 -0500

    art serializer and vote serializer work

--- app/serializers/art_serializer.rb
+++ app/serializers/art_serializer.rb
@@ -1,4 +1,4 @@
 class ArtSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :author, :born_died, :title, :date, :technique, :location, :url, :form, :style, :school, :timeframe
   has_many :votes
 end
--- app/serializers/vote_serializer.rb
+++ app/serializers/vote_serializer.rb
@@ -1,3 +1,3 @@
 class VoteSerializer < ActiveModel::Serializer
-  attributes :id, :author, :born_died, :title, :date, :technique, :location, :url, :form, :style, :school, :timeframe
+  attributes :id
 end
