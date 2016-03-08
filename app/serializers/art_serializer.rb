5df2659913a00722ec80f6ae8afba1d3749bb48f
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-08 16:34:54 -0500

    add my_vote method to art serializer

--- app/serializers/art_serializer.rb
+++ app/serializers/art_serializer.rb
@@ -1,4 +1,12 @@
 class ArtSerializer < ActiveModel::Serializer
   attributes :id, :author, :born_died, :title, :date, :technique, :location, :url, :form, :style, :school, :timeframe
   has_many :votes
+  has_one :my_vote
+
+  private
+
+  def my_vote
+    Vote.find_by(voteable_id: object.id, voter_id: scope.id)
+  end
+
 end
