0ecbdd285b33d939dfb2f10911b716de8db36461
RobertRuttenberg <rdruttenberg@gmail.com>
2016-04-06 15:48:22 -0400

    migrations run on schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401160537) do
+ActiveRecord::Schema.define(version: 20160406191458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20160401160537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "gender"
+    t.string   "age_range"
+    t.integer  "height"
+    t.integer  "width"
+    t.integer  "positionX"
+    t.integer  "positionY"
   end
 
   create_table "users", force: :cascade do |t|
