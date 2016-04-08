02ca6a6ef5c0015f229a394f8d4fc3f3ecdaf9e5
RobertRuttenberg <rdruttenberg@gmail.com>
2016-04-07 22:39:00 -0400

    nested face in art-serializer

--- app/serializers/art_serializer.rb
+++ app/serializers/art_serializer.rb
@@ -1,7 +1,9 @@
 class ArtSerializer < ActiveModel::Serializer
+  # TODO remove gender from here, after fixing frontend
   attributes :id, :author, :born_died, :title, :date, :technique, :location, :url, :form, :style, :school, :timeframe, :gender
   has_many :votes
-  has_one :my_vote
+  has_one :my_vote, :face
+# IDEA has many faces in future?
 
   private
 
@@ -11,5 +13,10 @@ class ArtSerializer < ActiveModel::Serializer
     end
     Vote.find_by(voteable_id: object.id, voter_id: scope.id)
   end
+
+  def face
+    {gender: object.gender, age_range: object.age_range, height: object.height, width: object.width, positionX: object.positionX, positionY: object.positionY}
+  end
+
 
 end
